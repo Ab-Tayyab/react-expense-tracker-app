1631fa77ccc0e64e81eb61f5121fc9ddcce6e7ee
abdullahtayyab894 <abdullahtayyab894@gmail.com>
2023-06-19 08:41:20 +0500

    changing

--- src/component/expense_Tracker/ExpenseTracker.jsx
+++ src/component/expense_Tracker/ExpenseTracker.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
 import { useContext } from 'react';
 import { GlobalContext } from '../context/Context';
 import { ListItem, Typography, Box, List } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Scrollbars } from 'react-custom-scrollbars-2';
-import EditIcon from '@mui/icons-material/Edit';
+import bg from './bg.jpg'
+import './ExpenseTracker.css'
 
 const ExpenseTracker = () => {
+
+    const [top, setTop] = useState(0);
+
+    const handleUpdate = (values) => {
+        const { top } = values;
+        setTop(top);
+    };
+
+    const renderView = ({ style, ...props }) => {
+        const trackColor = `rgb(${Math.round(255 - top * 255)}, ${Math.round(255 - top * 255)}, ${Math.round(255 - top * 255)})`;
+
+        const viewStyle = {
+            ...style,
+        };
+
+        const trackStyle = {
+            backgroundColor: trackColor,
+        };
+
+        return (
+            <div style={viewStyle} {...props}>
+                <div className="track" style={trackStyle} />
+            </div>
+        );
+    };
+
+    const renderThumb = ({ style, ...props }) => {
+        const thumbColor = `rgb(${Math.round(255 - top * 255)}, ${Math.round(255 - top * 255)}, ${Math.round(255 - top * 255)})`;
+
+        const thumbStyle = {
+            backgroundColor: thumbColor,
+        };
+
+        return <div style={{ ...style, ...thumbStyle }} {...props} />;
+    };
     // User input Data
     let { budgetData, expenseData } = useContext(GlobalContext);
     let { register, handleSubmit, reset } = useForm();
@@ -38,162 +74,91 @@ const ExpenseTracker = () => {
             balance = (budget - expense)
         }
     })
+
+    const balanceClassName = balance > 0 ? 'balance-positive' : 'balance-negative';
     return (
-        <>
-            <Box sx={{
-                background: "#F5F5F5",
-                padding: "20px"
-            }}>
-                <Box sx={{
-                    display: "flex",
-                    flexWrap: "wrap",
-                    justifyContent: "space-around"
-                }}>
-                    <Box sx={{
-                        width: "350px",
-                        background: "white",
-                        padding: "20px",
-                        borderRadius: "5px"
-                    }}>
-                        <Typography variant="h5">Budget</Typography>
-                        <form
-                            onSubmit={handleSubmit(budgetFormSubmited)}
-                        >
-                            <input {...register('budget')} type='number' placeholder='Enter Total Budget' required style={{
-                                width: "100%",
-                                height: "35px",
-                                margin: "10px 0px 10px 0px",
-                                borderRadius: "5px",
-                            }}></input> <br />
-                            <button style={{
-                                width: "200px",
-                                height: "35px",
-                                background: "#3C79F5",
-                                borderRadius: "5px",
-                                border: "none",
-                                color: "white"
-                            }}>Set Budget</button>
-                        </form>
-                    </Box>
-                    <Box sx={{
-                        width: "350px",
-                        background: "white",
-                        padding: "20px",
-                        borderRadius: "5px"
-                    }}>
-                        <Typography variant="h5">Add Transection</Typography>
-                        <form onSubmit={handleSubmit(amountFormSubmited)}>
-                            <input {...register('description')} type='text' placeholder='Enter Expense Description' required style={{
-                                width: "100%",
-                                height: "35px",
-                                margin: "10px 0px 10px 0px",
-                                borderRadius: "5px",
-                            }}></input>  <br />
-                            <input {...register('amount')} type='number' placeholder='Enter Amount' required style={{
-                                width: "100%",
-                                height: "35px",
-                                margin: "10px 0px 10px 0px",
-                                borderRadius: "5px",
-                            }}></input> <br />
-                            <button style={{
-                                width: "200px",
-                                height: "35px",
-                                background: "#3C79F5",
-                                borderRadius: "5px",
-                                border: "none",
-                                color: "white"
-                            }}>Add Transection</button>
-                        </form>
-                    </Box>
+        <div style={{ backgroundImage: `url(${bg})`, backgroundSize: 'cover', minHeight: "100vh", paddingBottom: "20px" }}>
+            <Typography variant="h3" sx={{
+                color: "#FFFFFF",
+                textAlign: "center",
+                padding:"20px 0px"
+            }}>Expense Tracker App</Typography>
+            <Box className="parent-child1">
+                <Box className="child1-subchild1">
+                    <Typography variant="h5" className='buget-heading'>Budget</Typography>
+                    <form
+                        onSubmit={handleSubmit(budgetFormSubmited)}
+                    >
+                        <input className='buget-input' {...register('budget')} type='number' placeholder='Enter Total Budget' required ></input> <br />
+                        <button className='buget-btn'>Set Budget</button>
+                    </form>
                 </Box>
-                <Box
-                    sx={{
-                        display: "flex",
-                        flexWrap: "wrap",
-                        justifyContent: "space-around",
-                        background: "#3C79F5",
-                        height: "100px",
-                        alignItems: "center",
-                        color: "white",
-                        textAlign: "center",
-                        marginTop: "20px",
-                        borderRadius: "5px",
-                    }}>
-                    <Box>
-                        <Typography>TotalBudget</Typography>
-                        <Typography>
-                            $: {
-                                budget.toFixed(2)
-                            }
-                        </Typography>
-                    </Box>
-                    <Box>
-                        <Typography>Expense</Typography>
-                        <Typography>
-                            {
-                                expense.toFixed(2)
-                            }
-                        </Typography>
-                    </Box>
-                    <Box>
-                        <Typography>Balance</Typography>
-                        <Typography>
-                            {
-                                balance.toFixed(2)
-                            }
-                        </Typography>
-                    </Box>
-                </Box>
-                <Box sx={{
-                    marginTop: "20px",
-                    background: "white",
-                    borderRadius: "5px",
-                    height: "190px",
-                    paddingBottom: "5px"
-                }}>
-                    <Typography variant="h4" sx={{
-                        textAlign: "center",
-                        paddingTop:"10px"
-                    }}>History</Typography>
-                    <Scrollbars style={{ height: 150 }}>
-                        <List sx={{
-                            borderLeftColor: "#3C79F5"
-                        }}>
-                            {
-                                mytransection.map((item, index) => {
-                                    return (
-                                        <ListItem>
-                                            <Box sx={{
-                                                display: "flex",
-                                                justifyContent: "space-around",
-                                                width: "100%",
-                                            }}>
-                                                <Typography sx={{
-                                                    borderLeft: "4px solid #3C79F5",
-                                                    paddingLeft: "15px"
-                                                }}>{item.description} </Typography>
-                                                <Typography>RS: {item.amount}</Typography>
-                                                <DeleteIcon sx={{
-                                                    color: "#3C79F5",
-                                                    "&:hover": {
-                                                        color: "red"
-                                                    },
-                                                }} onClick={
-                                                    () => {
-                                                        mytransection.splice(index, 1)
-                                                        dltData([...mytransection])
-                                                    }
-                                                } />
-                                            </Box>
-                                        </ListItem>
-                                    )
-                                })
-                            }
-                        </List>
-                    </Scrollbars>
+                <Box className="transection-des">
+                    <Typography variant="h5" className='buget-heading'>Add Transection</Typography>
+                    <form onSubmit={handleSubmit(amountFormSubmited)}>
+                        <input className='add-des buget-input' {...register('description')} type='text' placeholder='Enter Expense Description' required ></input>  <br />
+                        <input className='add-amount buget-input' {...register('amount')} type='number' placeholder='Enter Amount' required ></input> <br />
+                        <button className='add-btn buget-btn'>Add Transection</button>
+                    </form>
                 </Box>
             </Box>
-        </>
+            <Box className="parent-child2">
+                <Box>
+                    <Typography>Total Budget</Typography>
+                    <Typography>
+                        $: {
+                            budget.toFixed(2)
+                        }
+                    </Typography>
+                </Box>
+                <Box>
+                    <Typography>Expense</Typography>
+                    <Typography>
+                        {
+                            expense.toFixed(2)
+                        }
+                    </Typography>
+                </Box>
+                <Box className={balanceClassName}>
+                    <Typography>Balance</Typography>
+                    <Typography >
+                        {balance.toFixed(2)}
+                    </Typography>
+                </Box>
+            </Box>
+            <Box className="parent-child3">
+                <Typography variant="h4" className='child3-heading'>History</Typography>
+                <Scrollbars className='scrollbar' style={{ height: 150 }}
+                    renderView={renderView}
+                    renderThumbHorizontal={renderThumb}
+                    renderThumbVertical={renderThumb}
+                    onUpdate={handleUpdate}
+                >
+                    <List sx={{
+                        borderLeftColor: "#3C79F5"
+                    }}>
+                        {
+                            mytransection.map((item, index) => {
+                                return (
+                                    <ListItem>
+                                        <Box className="list-box">
+                                            <Typography className='list-des'>{item.description} </Typography>
+                                            <Typography className='list-amount'>RS: {item.amount}</Typography>
+                                            <DeleteIcon className="list-btn" onClick={
+                                                () => {
+                                                    mytransection.splice(index, 1)
+                                                    dltData([...mytransection])
+                                                }
+                                            } />
+                                        </Box>
+                                    </ListItem>
+                                )
+                            })
+                        }
+                    </List>
+                </Scrollbars>
+            </Box>
+        </div>
     )
 }
 
